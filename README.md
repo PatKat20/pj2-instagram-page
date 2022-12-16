5f8309c8f9b915b82b6a127c13e615ec0e77ced5
PatKatXD <100157955+PatKat20@users.noreply.github.com>
2022-12-16 16:45:51 -0300

    Sobe arquivos do projeto

---
+++ README.md
@@ -0,0 +1,20 @@
+# Projeto #02 - Instagram - DRIVEN EDUCATION BOOTCAMP
+
+<hr>
+
+## Descrição do Projeto
+
+```
+<p align="center">O desafio do projeto foi de replicar a página principal do instagram utilizando apenas tecnologias de front-end com layout responsivo.</p>
+```
+
+<hr>
+
+### Requisitos
+
+- [ ] Aplicar layout para *desktop*, seguindo layout fornecido no Figma;
+- [ ] Aplicar layout para *mobile*, seguindo layout fornecido no Figma;
+- [ ] O layout sem *sidebar* deve ser ativado quando a largura da tela for menor que 935px;
+- [ ] O layout para *mobil*e deve ser ativado quando a largura da tela for inferior a 614px;
+- [ ] Não é obrigatório que a *sidebar* fique fixa conforme o usuário desce na página como ocorre no Instagram (mas é um bônus).
+
---
+++ assets/js/script.js
@@ -0,0 +1,94 @@
+const apiUnsplash = (imagem, size) => `https://source.unsplash.com/${size}/?${imagem}`;
+
+const usersData = [
+    { userAvatar: "assets/img/stories/1.png", user: "9gag", post: "coding" },
+    { userAvatar: "assets/img/stories/2.png", user: "meowed", post: "cat" },
+    { userAvatar: "assets/img/stories/3.png", user: "barked", post: "harrypotter" },
+    { userAvatar: "assets/img/stories/4.png", user: "nathanwpyle...", post: "viagem" },
+    { userAvatar: "assets/img/stories/5.png", user: "wawawiwac...", post: "apartamento" },
+    { userAvatar: "assets/img/stories/6.png", user: "respondeai", post: "math" },
+    { userAvatar: "assets/img/stories/7.png", user: "filomoderna", post: "gato" },
+    { userAvatar: "assets/img/stories/8.png", user: "memeriago.", post: "show" },
+]
+
+const generateStoriesHTML = stories => {
+    return stories.reduce((accumulator, { userAvatar, user }) => {
+        return accumulator += `
+            <a href="#" class="story">
+                <img src="assets/img/stories/stories_background.jpg" class="story-img"></img>
+                <img src="${userAvatar}" class="story-user"></img>
+                <p>${user}</p>
+            </a>
+        `
+    }, "")
+}
+
+const generatePostsHTML = posts => {
+    return posts.reduce((accumulator,) => {
+        let randomNumber = Math.round(Math.random() * 7)
+        let randomUser = usersData[randomNumber]
+
+        return accumulator += `
+        <article class="posts">
+            <header class="header-post">
+                <div class="user-info flex-container">
+                    <img src="${randomUser.userAvatar}" style="width:32px;"></img>
+                    <p>${randomUser.user}</p>
+                </div>
+                <img src="/assets/img/dots.svg" style="width:20px; margin-right:18px;"></img>
+            </header>
+            <div class="post-img">
+            <picture>
+                <img src="${apiUnsplash(randomUser.post, "614x614")}"></img>
+            </picture>      
+            </div>
+
+            <footer>
+                <div class="icons-area flex-container">
+                    <div class="shared-icons flex-container">
+                        <a href="#">
+                            <img src="assets/img/heart.svg" alt="" style="width:20px;">
+                        </a>
+
+                        <a href="#">
+                            <img src="assets/img/chat.svg" alt="">
+                        </a>
+
+                        <a href="#">
+                            <img src="assets/img/send.svg" alt="">
+                        </a>
+                    </div>
+                    <div>
+                        <a href="#">
+                            <img src="assets/img/save.svg" alt="" class="save-icon">
+                        </a>
+                    </div>
+                </div>
+                <div class="comment-area">
+                    <img src="${randomUser.userAvatar}" style="width:20px;"></img> 
+                    <span> 
+                        Curtido por <b>${randomUser.user}</b> e <strong> outras ${Math.round(Math.random() * 100000).toLocaleString('pt-BR')}</strong>
+                    </span>
+                </div>
+            </footer>
+        </article>
+        `
+    }, "")
+}
+
+const insertStories = stories => {
+    const ul = document.querySelector("#stories-ul")
+    ul.innerHTML = stories
+}
+
+const insertPosts = posts => {
+    const postsContent = document.getElementById("posts-area");
+
+    postsContent.innerHTML = posts
+}
+
+const storiesInBox = generateStoriesHTML(usersData)
+const postsInBox = generatePostsHTML(usersData)
+
+insertStories(storiesInBox);
+insertPosts(postsInBox)
