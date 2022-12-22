ff4e969766b06fc3d7503856ad07b2a7f664a0f3
PatKatXD <100157955+PatKat20@users.noreply.github.com>
2022-12-22 15:57:02 -0300

    Adiciona mais stories com javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,15 @@ const usersData = [
     { userAvatar: "assets/img/stories/1.png", user: "9gag", post: "coding" },
     { userAvatar: "assets/img/stories/2.png", user: "meowed", post: "cat" },
     { userAvatar: "assets/img/stories/3.png", user: "barked", post: "harrypotter" },
-    { userAvatar: "assets/img/stories/4.png", user: "nathanwpyle...", post: "viagem" },
-    { userAvatar: "assets/img/stories/5.png", user: "wawawiwac...", post: "apartamento" },
+    { userAvatar: "assets/img/stories/4.png", user: "nathanwpyle", post: "viagem" },
+    { userAvatar: "assets/img/stories/5.png", user: "wawawiwac", post: "apartamento" },
     { userAvatar: "assets/img/stories/6.png", user: "respondeai", post: "math" },
     { userAvatar: "assets/img/stories/7.png", user: "filomoderna", post: "filosofia" },
     { userAvatar: "assets/img/stories/8.png", user: "memeriago.", post: "meme" },
+    { userAvatar: `${apiUnsplash("mickey", "66x66")}`, user: "Mickey", post: "mickey" },
+    { userAvatar: `${apiUnsplash("donald", "66x66")}`, user: "donald.", post: "donald" },
+    { userAvatar: `${apiUnsplash("duck", "66x66")}`, user: "duck", post: "duck" },
+    { userAvatar: `${apiUnsplash("dog", "66x66")}`, user: "pateta", post: "dog" },
 ]
 
 const generateStoriesHTML = stories => {
@@ -25,7 +29,7 @@ const generateStoriesHTML = stories => {
 
 const generatePostsHTML = posts => {
     return posts.reduce((accumulator, _) => {
-        let randomNumber = Math.round(Math.random() * 7)
+        let randomNumber = Math.round(Math.random() * (usersData.length - 1))
         let randomUser = usersData[randomNumber]
         let randomPost = apiUnsplash(usersData[randomNumber].post, "614x614")
 
